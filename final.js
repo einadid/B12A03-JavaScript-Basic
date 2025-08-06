943220ca711b44a96eaca58a0441fb7111a56f4d
Nadid <emamulislamnadid_cse30d@portcity.edu.bd>
2025-08-07 05:30:42 +0600

    Uplode Assignment

---
+++ final.js
@@ -0,0 +1,62 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+var result = area / 2;
+console.log(result);
+
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money >= 25000) {
+    console.log("Laptop");
+}
+else if (money >= 10000) {
+    console.log("Cycle");
+}
+else if (money >= 9999) {
+    console.log("Chocolate");
+}
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11 ;
+//write your code here
+for (var i = 1; i <= lastDay; i++) {
+    if (i % 3 !== 0) { 
+        console.log( i+ " - rest");
+    } else {
+        console.log( i+ " - medicine");
+    }
+}
+
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+if ( fileName.startsWith("#") || fileName.endsWith("pdf") || fileName.endsWith("docx") ) {
+    console.log("Store");
+}
+else {
+    console.log("Delete");
+}
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(email);
+
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+var currentSalary = startingSalary;
+var incrementSalary = 0.05;
+for (var i = 1; i <= experience; i++) {
+    var yearlyIncrement = currentSalary * incrementSalary;
+    currentSalary = currentSalary + yearlyIncrement;
+}
+console.log(currentSalary.toFixed(2)); 
